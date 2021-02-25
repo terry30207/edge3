b6d16afc2f372d973172eea105f945d7abfa2b06
terry30207 <32173096+terry30207@users.noreply.github.com>
2021-02-25 23:38:48 +0800

    Add files via upload

---
+++ edge3.py
@@ -0,0 +1,22 @@
+import numpy
+import tensorflow
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import load_model
+from pathlib import Path
+
+
+numpy.random.seed(7)
+print()
+(X_test, y_test),(X_train, y_train) = mnist.load_data()
+X_train = X_train / 255
+X_test = X_test / 255
+model = load_model(str(Path(__file__).resolve().parent.parent)+'\\aggr\\global.h5')
+dataset2_x = X_train
+dataset2_y = y_train
+model.fit(dataset2_x, dataset2_y, epochs=10, batch_size=200, verbose=2)
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+model.save("edge3.h5")
